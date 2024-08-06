4eee83f0601c197da2261460dd6412156f43e68f
Shreyas719 <shreyasbhagwat4055@gmail.com>
2024-08-06 13:02:20 +0800

    Initial commit

---
+++ resources/views/layouts/admin.blade.php
@@ -0,0 +1,67 @@
+<!doctype html>
+<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
+
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+
+    <!-- CSRF Token -->
+    <meta name="csrf-token" content="{{ csrf_token() }}">
+
+    <title>{{ config('app.name', 'Laravel') }}</title>
+
+
+
+    <!-- plugins:css -->
+    <link rel="stylesheet" href="{{asset('admin_assets/vendors/mdi/css/materialdesignicons.min.css')}}">
+    <link rel="stylesheet" href="{{asset('admin_assets/vendors/css/vendor.bundle.base.css')}}">
+    <!-- endinject -->
+    <!-- plugin css for this page -->
+    <link rel="stylesheet" href="{{asset('admin_assets/vendors/datatables.net-bs4/dataTables.bootstrap4.css')}}">
+    <!-- End plugin css for this page -->
+    <!-- inject:css -->
+    <link rel="stylesheet" href="{{asset('admin_assets/css/style.css')}}">
+    <!-- endinject -->
+    <link rel="shortcut icon" href="{{asset('admin_assets/images/favicon.ico')}}" />
+
+    @livewireStyles
+</head>
+
+<body>
+
+
+    <div class="container-scroller">
+        @include('layouts.inc.admin.navbar')
+        <div class="container-fluid page-body-wrapper">
+            @include('layouts.inc.admin.sidebar')
+
+            <div class="main-panel">
+                <div class="content-wrapper">
+                        @yield('content')
+                </div>
+            </div>
+        </div>
+    </div>
+
+    <!-- plugins:js -->
+    <script src="{{asset('admin_assets/vendors/js/vendor.bundle.base.js')}}"></script>
+    <!-- endinject -->
+    <!-- Plugin js for this page-->
+    <script src="{{asset('admin_assets/vendors/chart.js/chart.umd.js')}}"></script>
+    <script src="{{asset('admin_assets/vendors/datatables.net/jquery.dataTables.js')}}"></script>
+    <script src="{{asset('admin_assets/vendors/datatables.net-bs4/dataTables.bootstrap4.js')}}"></script>
+    <!-- End plugin js for this page-->
+    <!-- inject:js -->
+    <script src="{{asset('admin_assets/js/off-canvas.js')}}"></script>
+    <script src="{{asset('admin_assets/js/hoverable-collapse.js')}}"></script>
+    <script src="{{asset('admin_assets/js/template.js')}}"></script>
+    <!-- endinject -->
+    <!-- Custom js for this page-->
+    <script src="{{asset('admin_assets/js/dashboard.js')}}"></script>
+
+
+
+    @livewireScripts
+</body>
+
+</html>
